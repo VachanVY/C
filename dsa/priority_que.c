8dfe1544f37c7cb39231a0e6172ad30e23404f20
Vachan V Y <vachanvy05@gmail.com>
2024-10-24 00:04:24 +0530

    fixed error in priority que

--- dsa/priority_que.c
+++ dsa/priority_que.c
@@ -10,7 +10,7 @@ typedef struct {
 } Queue;
 
 Queue* init(int num_ques, const size_t qmaxlen){
-    Queue* pque; // array of Queues
+    Queue* pque = (Queue*)malloc(num_ques * sizeof(Queue)); // array of Queues
 
     for(int i=0; i<num_ques; i++){
         pque[i].que = (int *)malloc(sizeof(int)*qmaxlen);
@@ -25,6 +25,7 @@ void del(Queue* pque, size_t num_ques){ // input: array of Queues
     for(int i=0; i<num_ques; i++){
         free(pque[i].que);
     }
+    free(pque);
 }
 
 void insert_ascending(
@@ -59,33 +60,46 @@ void print_pque(
     size_t num_ques
 ){
     for(int i=0; i<num_ques; i++){
-        printf("Qnum: %i [", i+1);
-        for(int j=pque[i].front; j<=pque[i].rear; j++){
-            printf("%i", pque[i].que[j]);
+        printf("Qnum %i: [", i);
+        if(pque[i].rear>=pque[i].front){
+            for(int j=pque[i].front; j<=pque[i].rear; j++){
+                printf("%i, ", pque[i].que[j]);
+            }
         }
         printf("]\n");
     }
 }
 
 int main(){
-    size_t num_ques = 3; unsigned int maxlen = 5;
+    size_t num_ques = 3; size_t maxlen = 5;
     Queue* pque =  init(num_ques, maxlen);
-    for(int i=0; i<num_ques; i++){
+    print_pque(pque, num_ques);
+    printf("\n\n\nfor loop inserting\n");
+
+    for(size_t i=0; i<num_ques; i++){
         insert_ascending(pque, i, 23+i);
         insert_ascending(pque, i, 3232+i);
         insert_ascending(pque, i, 54554+i);
         insert_ascending(pque, i, 32324+i);
         insert_ascending(pque, i, 5252+i);
-        insert_ascending(pque, i, 6755+i);
     }
     print_pque(pque, num_ques);
-    // delete_element(pque, 1);
-    // print_pque(pque, num_ques);
-    // delete_element(pque, 2);
-    // print_pque(pque, num_ques);
-    // delete_element(pque, 0);
-    // print_pque(pque, num_ques);
-    // delete_element(pque, 1);
+
+    printf("\n\n\ndeleting from qnum 1\n");
+    delete_element(pque, 1);
+    print_pque(pque, num_ques);
+
+    printf("\n\n\ndeleting from qnum 2\n");
+    delete_element(pque, 2);
+    print_pque(pque, num_ques);
+
+    printf("\n\n\ndeleting from qnum 0\n");
+    delete_element(pque, 0);
+    print_pque(pque, num_ques);
+
+    printf("\n\n\ndeleting from qnum 0\n");
+    delete_element(pque, 1);
+    print_pque(pque, num_ques);
 
     del(pque, num_ques);
 }
